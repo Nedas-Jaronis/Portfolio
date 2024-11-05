90aafc03c65c2af29b7b6aeda405293fd2a49b23
Nedas Jaronis <148506545+Nedas-Jaronis@users.noreply.github.com>
2024-11-04 21:59:01 -0500

    Arrow Changes

--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -42,20 +42,6 @@ export default function Intro() {
           <span className="sr-only">GitHub Profile</span>
         </Link>
       </div>
-      <div className="flex justify-center mt-16">
-        <svg
-          className="w-6 h-6 animate-bounce text-blue-600"
-          fill="none"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-        </svg>
-      </div>
-      
     </section>
   )
 }
